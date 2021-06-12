5609fe321af4ea305fe1f24d066906bb658683b3
Michal Plevko <plevko.michal@gmail.com>
2021-06-12 21:34:14 +0200

    Hello angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular';
+
+  htmlExample = '<div>Hello angular</div>';
+
+  getName(): string {
+    return 'Angular function';
+  }
 }
