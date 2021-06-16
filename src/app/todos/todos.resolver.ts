a941a7bebb302df88dd12ec3cdec076d79f83139
Michal Plevko <plevko.michal@gmail.com>
2021-06-16 19:57:36 +0200

    Observable operators

--- src/app/todos/todos.resolver.ts
+++ src/app/todos/todos.resolver.ts
@@ -2,13 +2,32 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Resolve } from '@angular/router';
 import { Observable } from 'rxjs';
+import { delay, finalize, map } from 'rxjs/operators';
+import { AlertService } from '../alert/alert.service';
 import { Todo } from './todo.model';
 
 @Injectable()
 export class TodosResolver implements Resolve<Todo[]> {
-  constructor(private readonly http: HttpClient) {}
+  constructor(
+    private readonly alertService: AlertService,
+    private readonly http: HttpClient
+  ) {}
 
   resolve(): Observable<Todo[]> {
-    return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/posts');
+    this.alertService.addInfoAlert('Loading todos');
+    return this.http
+      .get<Todo[]>('https://jsonplaceholder.typicode.com/posts')
+      .pipe(
+        delay(2000),
+        map((todos) => {
+          return todos.map((todo) => {
+            todo.title += '🥳';
+            return todo;
+          });
+        }),
+        finalize(() => {
+          this.alertService.addInfoAlert('Todos loaded');
+        })
+      );
   }
 }
