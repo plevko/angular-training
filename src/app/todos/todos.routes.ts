cda6a030ecfaa4766f2e4edc6162be8b5adc486b
Michal Plevko <plevko.michal@gmail.com>
2021-06-16 19:32:41 +0200

    Todos with http call

---
+++ src/app/todos/todos.routes.ts
@@ -0,0 +1,9 @@
+import { TodosComponent } from './todos.component';
+import { Routes } from '@angular/router';
+
+export const todosRoutes: Routes = [
+  {
+    path: '',
+    component: TodosComponent,
+  },
+];
