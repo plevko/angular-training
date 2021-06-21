02f18ea7bd46316212202f067091ea6b29985384
Michal Plevko <plevko.michal@gmail.com>
2021-06-21 17:11:55 +0200

    Date slovak locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { LoginService } from './shared/login.service';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AlertListComponent } from './alert/alert-list.component';
@@ -13,6 +13,11 @@ import { MenuComponent } from './menu/menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EmojiAdderModule } from 'capco-shared';
 
+import localeSk from '@angular/common/locales/sk';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeSk);
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,8 +27,17 @@ import { EmojiAdderModule } from 'capco-shared';
     AlertListComponent,
     MenuComponent,
   ],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes), BrowserAnimationsModule, EmojiAdderModule],
-  providers: [AlertService, LoginService],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    EmojiAdderModule,
+  ],
+  providers: [
+    AlertService,
+    LoginService,
+    { provide: LOCALE_ID, useValue: 'sk-SK' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/hello/hello-list.component.ts
+++ src/app/hello/hello-list.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
 
 @Component({
   templateUrl: './hello-list.component.html',
 })
 export class HelloListComponent {
   name = 'Main name';
+  date = new Date();
 
   getName(): string {
     return 'Miroslav';
   }
+
+  constructor(@Inject(LOCALE_ID) public locale: string) {}
 }
